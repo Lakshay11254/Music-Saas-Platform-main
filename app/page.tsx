3f5317a0e724d7c5292a6f6e11df0aa81ab8edd6
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-10-01 05:24:57 -0400

    setting up page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import Appbar from "./component/Appbar";
 
 export default function Home() {
   return (
-    <div className="">
-    //have to setup the .env file
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     <Appbar/>
     </div>
   );
