5d73f222356b0c62726fee6b03e156ef3e5fb527
Lakshay Malhotra <68064745+Lakshay11254@users.noreply.github.com>
2024-10-16 08:36:09 -0400

    Update Appbar.tsx

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,27 +1,4 @@
-// "use client";
 
-// import { signIn, signOut, useSession } from 'next-auth/react'
-
-
-// export function Appbar() {
-//     const session = useSession();
-
-//     return <div>
-//         <div className='flex justify-between'>
-//             <div>
-//                 Navbar will be placed here
-//             </div>
-//             <div>
-//                 {session.data?.user && <button className='m-2 p-2 bg-blue-400' onClick={() => signOut()}>Sign Out</button>}
-//                 {!session.data?.user && <button className='m-2 p-2 bg-blue-400' onClick={() => signIn()}>Sign In</button>}
-//             </div>
-//         </div>
-//     </div>
-
-
-// }
-
-// export default Appbar
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
